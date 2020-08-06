37749c005a8f403ae63bb2524ead2888c97a1347
Carlos Eduardo Thompson <carlosethompson@gmail.com>
2020-08-06 13:02:26 +0100

    Lecture 27: Adding  A Custom Function

---
+++ assets/scripts/variableNaming.js
@@ -0,0 +1,16 @@
+// Variable Naming 
+
+//Allowed
+
+let currentResult; // BestPractice: camelcase
+let ageGroup5; // only letters and digits
+let $kindOfSpecial; // start with $
+let _intervalValue; //start with _
+
+// Not Recommend
+let user_name; // Allowed but bad practice
+
+// Not Allowed
+let 21Player; // Start with digit
+let user-b; // No special character!
+let let; // keyword not allowed
